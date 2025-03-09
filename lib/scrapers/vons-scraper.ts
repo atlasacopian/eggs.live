132a6e53d72b228f64dd5fed7670479cf2ef3cb9
atlasacopian <atlasacopian@gmail.com>
2025-03-08 21:37:56 -0800

    Update vons-scraper.ts

--- lib/scrapers/vons-scraper.ts
+++ lib/scrapers/vons-scraper.ts
@@ -1,4 +1,4 @@
-import { BaseScraper } from "./base-scraper"
+import { BaseScraper } from "./base-scraper";
 
 export class VonsScraper extends BaseScraper {
   constructor() {
@@ -6,9 +6,8 @@ export class VonsScraper extends BaseScraper {
       name: "Vons",
       regularEggUrl: "https://www.vons.com/shop/product-details.970041.html",
       regularEggSelector: ".product-price",
-      organicEggUrl: null, // Add organic URL if available
+      organicEggUrl: null,
       organicEggSelector: null,
-    })
+    });
   }
 }
-
