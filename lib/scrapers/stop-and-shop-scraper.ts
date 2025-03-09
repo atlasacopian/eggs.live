6f9299606b3af669651bfad4f625eaf2bbace999
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:50:09 -0800

    Create stop-and-shop-scraper.ts

---
+++ lib/scrapers/stop-and-shop-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class StopAndShopScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Stop & Shop",
+      regularEggUrl: "https://stopandshop.com/product-search/large%20eggs",
+      regularEggSelector: ".current-price",
+      organicEggUrl: null,
+      organicEggSelector: null,
+    })
+  }
+}
+
