d55a28185a46492c7e4697edffafdae8f5529afa
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:50:20 -0800

    Create sprouts-scraper.ts

---
+++ lib/scrapers/sprouts-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class SproutsScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Sprouts",
+      regularEggUrl: "https://shop.sprouts.com/product/33795/cage-free-large-grade-a-brown-eggs",
+      regularEggSelector: ".product-price",
+      organicEggUrl: "https://shop.sprouts.com/product/33796/sprouts-organic-cage-free-large-grade-a-brown-eggs",
+      organicEggSelector: ".product-price",
+    })
+  }
+}
+
