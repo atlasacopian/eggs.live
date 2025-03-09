d60e53afe3b9c7f1261739875678f3d4397b2821
atlasacopian <atlasacopian@gmail.com>
2025-03-08 21:36:57 -0800

    Update meijer-scraper.ts

--- lib/scrapers/meijer-scraper.ts
+++ lib/scrapers/meijer-scraper.ts
@@ -1,4 +1,4 @@
-import { BaseScraper } from "./base-scraper"
+import { BaseScraper } from "./base-scraper";
 
 export class MeijerScraper extends BaseScraper {
   constructor() {
@@ -8,7 +8,6 @@ export class MeijerScraper extends BaseScraper {
       regularEggSelector: ".price-container .price",
       organicEggUrl: null,
       organicEggSelector: null,
-    })
+    });
   }
 }
-
