ad0dd39bdfe97c92213b0ed41cf98ba3cecbd85d
atlasacopian <atlasacopian@gmail.com>
2025-03-08 21:38:12 -0800

    Update winn-dixie-scraper.ts

--- lib/scrapers/winn-dixie-scraper.ts
+++ lib/scrapers/winn-dixie-scraper.ts
@@ -1,15 +1,13 @@
-import { BaseScraper } from "./base-scraper"
+import { BaseScraper } from "./base-scraper";
 
 export class WinnDixieScraper extends BaseScraper {
   constructor() {
     super({
       name: "Winn-Dixie",
-      regularEggUrl:
-        "https://www.winndixie.com/shop/product-details/winn-dixie-grade-a-large-eggs-12-ct-00021140980018",
+      regularEggUrl: "https://www.winndixie.com/shop/product-details/winn-dixie-grade-a-large-eggs-12-ct-00021140980018",
       regularEggSelector: ".price",
       organicEggUrl: null,
       organicEggSelector: null,
-    })
+    });
   }
 }
-
