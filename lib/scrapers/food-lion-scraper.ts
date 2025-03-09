f5cdc62b9029b2d441fb44ef4294aa11d8e6ef94
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:53:57 -0800

    Create food-lion-scraper.ts

---
+++ lib/scrapers/food-lion-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class FoodLionScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Food Lion",
+      regularEggUrl: "https://shop.foodlion.com/product/large-white-eggs/00041130007015",
+      regularEggSelector: ".product-price",
+      organicEggUrl: null,
+      organicEggSelector: null,
+    })
+  }
+}
+
