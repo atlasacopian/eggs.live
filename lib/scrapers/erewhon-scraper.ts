4fa8b37447a83fd30c49b6323ca4b79f8dcf9722
atlasacopian <atlasacopian@gmail.com>
2025-03-08 21:36:11 -0800

    Update erewhon-scraper.ts

--- lib/scrapers/erewhon-scraper.ts
+++ lib/scrapers/erewhon-scraper.ts
@@ -1,4 +1,4 @@
-import { BaseScraper } from "./base-scraper"
+import { BaseScraper } from "./base-scraper";
 
 export class ErewhonScraper extends BaseScraper {
   constructor() {
@@ -8,7 +8,6 @@ export class ErewhonScraper extends BaseScraper {
       regularEggSelector: ".product-price .price",
       organicEggUrl: null,
       organicEggSelector: null,
-    })
+    });
   }
 }
-
