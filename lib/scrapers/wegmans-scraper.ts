0dfa4a76a3ea43ea771c53e6728c465f5e9d238d
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:49:37 -0800

    Create wegmans-scraper.ts

---
+++ lib/scrapers/wegmans-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class WegmansScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Wegmans",
+      regularEggUrl: "https://shop.wegmans.com/product/46709/wegmans-grade-a-large-eggs",
+      regularEggSelector: '[data-testid="product-price"]',
+      organicEggUrl: null,
+      organicEggSelector: null,
+    })
+  }
+}
+
