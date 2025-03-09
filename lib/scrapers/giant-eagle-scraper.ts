1bce49c28f8a351014f55a25c4009c20624b35db
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:54:36 -0800

    Create giant-eagle-scraper.ts

---
+++ lib/scrapers/giant-eagle-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class GiantEagleScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Giant Eagle",
+      regularEggUrl: "https://shop.gianteagle.com/waterworks/product/00041130007015",
+      regularEggSelector: ".product__price",
+      organicEggUrl: null,
+      organicEggSelector: null,
+    })
+  }
+}
+
