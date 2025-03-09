dca6eb96173ab251bef55b34eecc7381791fa671
atlasacopian <atlasacopian@gmail.com>
2025-03-08 22:04:10 -0800

    Update scheduler.ts

--- lib/scrapers/scheduler.ts
+++ lib/scrapers/scheduler.ts
@@ -1,40 +1,6 @@
 import { prisma } from "../db";
 import { scrapeEggPrices } from "../scrapers";
 
-// Simple scheduler class for running scrapers
-export class ScraperScheduler {
-  private interval: NodeJS.Timeout | null = null;
-  
-  constructor(private scrapeFunction: () => Promise<void>, private intervalMinutes: number = 1440) {
-    // Default to daily (1440 minutes)
-  }
-  
-  start() {
-    if (this.interval) {
-      this.stop();
-    }
-    
-    // Run immediately
-    this.scrapeFunction();
-    
-    // Then schedule
-    const intervalMs = this.intervalMinutes * 60 * 1000;
-    this.interval = setInterval(() => {
-      this.scrapeFunction();
-    }, intervalMs);
-    
-    console.log(`Scheduler started. Will run every ${this.intervalMinutes} minutes.`);
-  }
-  
-  stop() {
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.interval = null;
-      console.log('Scheduler stopped.');
-    }
-  }
-}
-
 // Function to run the daily scraping
 export async function scheduleDailyScraping() {
   try {
