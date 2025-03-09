434f799b76ed9bd9ae0c7082361d1a19cbab0c5e
atlasacopian <atlasacopian@gmail.com>
2025-03-08 20:51:15 -0800

    Create publix-scraper.ts

---
+++ lib/scrapers/publix-scraper.ts
@@ -0,0 +1,14 @@
+import { BaseScraper } from "./base-scraper"
+
+export class PublixScraper extends BaseScraper {
+  constructor() {
+    super({
+      name: "Publix",
+      regularEggUrl: "https://www.publix.com/pd/publix-eggs-large/RIO-PCI-114195",
+      regularEggSelector: ".reg-price, .sale-price",
+      organicEggUrl: null,
+      organicEggSelector: null,
+    })
+  }
+}
+
