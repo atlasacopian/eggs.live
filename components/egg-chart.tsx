5d6d6ddf340f3b2c88b4e55fd62bcce3543d4a70
atlasacopian <atlasacopian@gmail.com>
2025-03-09 22:25:35 -0700

    Update egg-chart.tsx

--- components/egg-chart.tsx
+++ components/egg-chart.tsx
@@ -17,6 +17,11 @@ export default function EggChart({ data, title, dataSource }: EggChartProps) {
   const priceRange = maxPrice - minPrice
 
   const styles = {
+    wrapper: {
+      width: "100%",
+      overflowX: "hidden",
+      padding: "0 10px",
+    },
     graph: {
       width: "100%",
       height: "300px",
@@ -50,106 +55,108 @@ export default function EggChart({ data, title, dataSource }: EggChartProps) {
   const reversedData = [...data].reverse()
 
   return (
-    <div style={styles.graph}>
-      {/* X and Y axes */}
-      <div
-        style={{
-          position: "absolute",
-          left: "50px",
-          top: "20px",
-          bottom: "50px",
-          width: "2px",
-          backgroundColor: "#00ff00",
-        }}
-      ></div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "50px",
-          left: "50px",
-          right: "20px",
-          height: "2px",
-          backgroundColor: "#00ff00",
-        }}
-      ></div>
+    <div style={styles.wrapper}>
+      <div style={styles.graph}>
+        {/* X and Y axes */}
+        <div
+          style={{
+            position: "absolute",
+            left: "50px",
+            top: "20px",
+            bottom: "50px",
+            width: "2px",
+            backgroundColor: "#00ff00",
+          }}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "50px",
+            left: "50px",
+            right: "20px",
+            height: "2px",
+            backgroundColor: "#00ff00",
+          }}
+        ></div>
 
-      {/* Price labels */}
-      <div
-        style={{
-          position: "absolute",
-          left: "20px",
-          top: "20px",
-          fontSize: "14px",
-        }}
-      >
-        ${maxPrice.toFixed(2)}
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          left: "20px",
-          bottom: "40px",
-          fontSize: "14px",
-        }}
-      >
-        ${minPrice.toFixed(2)}
-      </div>
+        {/* Price labels */}
+        <div
+          style={{
+            position: "absolute",
+            left: "20px",
+            top: "20px",
+            fontSize: "14px",
+          }}
+        >
+          ${maxPrice.toFixed(2)}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            left: "20px",
+            bottom: "40px",
+            fontSize: "14px",
+          }}
+        >
+          ${minPrice.toFixed(2)}
+        </div>
 
-      {/* Graph points and lines */}
-      {reversedData.map((point, index) => {
-        const x = 50 + (index / (data.length - 1)) * (100 - 10) + "%"
-        const y = 100 - ((point.price - minPrice) / priceRange) * 100 + "%"
+        {/* Graph points and lines */}
+        {reversedData.map((point, index) => {
+          const x = 50 + (index / (data.length - 1)) * (100 - 10) + "%"
+          const y = 100 - ((point.price - minPrice) / priceRange) * 100 + "%"
 
-        return (
-          <div key={index}>
-            {/* Point */}
-            <div
-              style={{
-                ...styles.graphPoint,
-                left: x,
-                bottom: y,
-              }}
-            ></div>
-
-            {/* Line to next point */}
-            {index < data.length - 1 && (
+          return (
+            <div key={index}>
+              {/* Point */}
               <div
                 style={{
-                  position: "absolute",
+                  ...styles.graphPoint,
                   left: x,
                   bottom: y,
-                  width: (1 / (data.length - 1)) * (100 - 10) + "%",
-                  height: "2px",
-                  backgroundColor: "#00ff00",
-                  transform: "translateY(1px)",
-                  transformOrigin: "left bottom",
-                  rotate:
-                    Math.atan2(
-                      ((reversedData[index + 1].price - minPrice) / priceRange) * 100 -
-                        ((point.price - minPrice) / priceRange) * 100,
-                      100 / (data.length - 1),
-                    ) + "rad",
                 }}
               ></div>
-            )}
 
-            {/* Date label - only show every other month for clarity */}
-            {index % 2 === 0 && (
-              <div
-                style={{
-                  ...styles.graphLabel,
-                  left: x,
-                  bottom: "30px",
-                }}
-              >
-                {point.date.split("/")[0]}/{point.date.split("/")[2].substring(2)}
-              </div>
-            )}
-          </div>
-        )
-      })}
+              {/* Line to next point */}
+              {index < data.length - 1 && (
+                <div
+                  style={{
+                    position: "absolute",
+                    left: x,
+                    bottom: y,
+                    width: (1 / (data.length - 1)) * (100 - 10) + "%",
+                    height: "2px",
+                    backgroundColor: "#00ff00",
+                    transform: "translateY(1px)",
+                    transformOrigin: "left bottom",
+                    rotate:
+                      Math.atan2(
+                        ((reversedData[index + 1].price - minPrice) / priceRange) * 100 -
+                          ((point.price - minPrice) / priceRange) * 100,
+                        100 / (data.length - 1),
+                      ) + "rad",
+                  }}
+                ></div>
+              )}
 
-      {dataSource && <div style={styles.dataSource}>Source: {dataSource}</div>}
+              {/* Date label - only show every other month for clarity */}
+              {index % 2 === 0 && (
+                <div
+                  style={{
+                    ...styles.graphLabel,
+                    left: x,
+                    bottom: "30px",
+                  }}
+                >
+                  {point.date.split("/")[0]}/{point.date.split("/")[2].substring(2)}
+                </div>
+              )}
+            </div>
+          )
+        })}
+
+        {dataSource && <div style={styles.dataSource}>Source: {dataSource}</div>}
+      </div>
     </div>
   )
 }
