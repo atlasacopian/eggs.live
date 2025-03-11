abe7824e9b9a56d557fb8fc1579a1df8ed6d5d07
atlasacopian <atlasacopian@gmail.com>
2025-03-10 21:58:38 -0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,11 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(date: Date): string {
-  return new Intl.DateTimeFormat("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  }).format(date)
-}
-
