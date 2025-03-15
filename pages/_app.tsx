40d990d125f2a2d298221331c579e17959c301cc
atlasacopian <atlasacopian@gmail.com>
2025-03-14 18:57:23 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app"
+import { ThemeProvider } from "@/components/theme-provider"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-amber-50 to-white">
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="min-h-screen bg-gradient-to-b from-amber-50 to-white dark:from-gray-900 dark:to-gray-800">
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   )
 }
 
