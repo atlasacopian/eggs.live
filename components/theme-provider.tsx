55f96e2f317de11d443110518b6278be98c7e2b8
atlasacopian <atlasacopian@gmail.com>
2025-03-14 20:46:51 -0700

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,25 +1,12 @@
 "use client"
 
 import type * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 type ThemeProviderProps = {
   children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
 }
 
-export function ThemeProvider({
-  children,
-  attribute = "class",
-  defaultTheme = "system",
-  enableSystem = true,
-}: ThemeProviderProps) {
-  return (
-    <NextThemesProvider attribute={attribute} defaultTheme={defaultTheme} enableSystem={enableSystem}>
-      {children}
-    </NextThemesProvider>
-  )
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return <>{children}</>
 }
 
