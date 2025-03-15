426b9715fcf54ce51cc1bfd69777f0a035e75550
atlasacopian <atlasacopian@gmail.com>
2025-03-14 20:53:09 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  swcMinify: true,
 }
 
-export default nextConfig
+module.exports = nextConfig
 
